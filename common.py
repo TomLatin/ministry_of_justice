46a3ba1eee482ee64b86d6434758e945df0ec2b9
TomLatin <57855070+TomLatin@users.noreply.github.com>
2024-09-30 10:13:51 +0300

    done pre_data_cleaning.py and add common.py

---
+++ common.py
@@ -0,0 +1,2 @@
+DATA_FILEPATH='test_data_engineer.csv'
+DATA_FILE_ENCODING = 'ISO-8859-8'
--- pre_data_cleaning.py
+++ pre_data_cleaning.py
@@ -1,12 +1,35 @@
+from pathlib import Path
+
 import pandas as pd
 from ydata_profiling import ProfileReport
 
-# Load the data
-data = pd.read_csv(filepath_or_buffer='test_data_engineer.csv', encoding='ISO-8859-8')
-print(f"data info:\n{data.info()}\n")
-print(f"data describe:\n{data.describe()}\n")
-print(f"data isnull:\n{data.isnull().sum()}\n")
-print(f"data duplicated:\n{data.duplicated().sum()}\n")
+from common import DATA_FILEPATH, DATA_FILE_ENCODING
 
-profile = ProfileReport(data, title="Profiling Report")
-profile.to_file("profiling_report.html")
+
+def load_data(filepath: Path, encoding: str) -> pd.DataFrame:
+    return pd.read_csv(filepath_or_buffer=filepath, encoding=encoding)
+
+
+def data_status(data: pd.DataFrame) -> None:
+    print(f"data info:")
+    print(f"{data.info()}\n")
+    print(f"data describe:")
+    print(f"{data.describe()}\n")
+    print(f"data isnull:")
+    print(f"{data.isnull().sum()}\n")
+    print(f"data duplicated:")
+    print(f"{data.duplicated().sum()}\n")
+    # # Check how many rows have all None/NaN values
+    num_all_none_rows = data.isnull().all(axis=1).sum()
+    print("Number of rows where all values are None/NaN:", num_all_none_rows)
+
+
+def build_profile(data: pd.DataFrame, output_file: str) -> None:
+    profile = ProfileReport(data)
+    profile.to_file(output_file)
+
+
+if __name__ == '__main__':
+    df = load_data(filepath=DATA_FILEPATH, encoding=DATA_FILE_ENCODING)
+    data_status(data=df)
+    build_profile(data=df, output_file='profiling_report.html')
